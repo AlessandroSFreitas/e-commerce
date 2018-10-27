3d8e035274740751af7994acad84ca9acc6e13d0
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2018-10-27 08:00:14 -0300

    configuring access to the backoffice auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   namespace :site do
     get 'home', to: 'home#index'
   end
+
+  get 'backoffice', to: 'backoffice/dashboard#index'
+
   namespace :backoffice do
     get 'dashboard', to: 'dashboard#index'
   end
+
   devise_for :admins
   devise_for :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
