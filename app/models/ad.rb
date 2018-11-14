f9a54708b04a1be5a4d3d32c8764d51a2eb75102
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2018-11-14 13:07:51 -0200

    initial config active storage

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -5,8 +5,7 @@ class Ad < ApplicationRecord
   scope :descending_order, ->(quantity = 12) { limit(quantity).order(created_at: :desc) }
   scope :to_the, ->(member) { where(member_id: member.id) }
 
-  has_attached_file :picture, styles: { medium: "700x400#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  has_one_attached :picture
 
   monetize :price_cents
 end
