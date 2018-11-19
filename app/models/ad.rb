7628ca9c7086ce766247c1928117e58d9b7970fb
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2018-11-19 12:59:36 -0200

    fix images dimensions

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -8,7 +8,7 @@ class Ad < ApplicationRecord
   scope :descending_order, ->(quantity = 12) { limit(quantity).order(created_at: :desc) }
   scope :to_the, ->(member) { where(member_id: member.id) }
 
-  has_attached_file :picture, styles: { medium: "200x200#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, styles: { medium: "286x258#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
   monetize :price_cents
